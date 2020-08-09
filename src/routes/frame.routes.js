e5ea0c4bcf2bd9e6fbce2a0790fdfa2575b6b0dd
Waldener Monteiro <jjrr019@gmail.com>
2020-08-08 21:39:52 -0300

    refactor(frames): alterando o nome dos métodos

--- src/routes/frame.routes.js
+++ src/routes/frame.routes.js
@@ -4,8 +4,8 @@ const frameController = require('../controllers/frame.controller');
 // ==> Definindo as rotas do CRUD - 'Frame':
 
 router.get('/frames', frameController.listAllFrames);
-router.get('/frame/:id', frameController.findFrameById);
+router.get('/frame/:id', frameController.findFrame);
 router.post('/frame', frameController.createFrame);
 router.put('/frame', frameController.updateFrame);
-router.delete('/frame', frameController.deleteFrame);
+router.delete('/frame/:id', frameController.deleteFrame);
 module.exports = router;
